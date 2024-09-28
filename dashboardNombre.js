4bf6e6848ff5d0e9d97b91301b6a20181d641666
Miguelperdomo <miguelperdomo2004@gmail.com>
2024-09-27 20:18:51 -0500

    Arreglos en los archivos html y js

---
+++ dashboardNombre.js
@@ -0,0 +1,32 @@
+// Este archivo se ejecutará en el cliente y no en el servidor
+
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault(); 
+
+    const email = document.getElementById('loginEmail').value;
+    const password = document.getElementById('loginPassword').value;
+
+    fetch('http://localhost:3000/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ email, password })
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Credenciales inválidas');
+        }
+        return response.json();
+    })
+    .then(data => {
+        console.log('Inicio de sesión exitoso', data);
+        localStorage.setItem('nombreUsuario', data.nombreUsuario);
+        console.log('Nombre de usuario almacenado:', data.nombreUsuario); // Agrega este log
+        window.location.href = 'dashboard.html'; 
+    })
+    .catch(error => {
+        console.error('Error:', error);
+        alert('Error al iniciar sesión: ' + error.message);
+    });
+});
